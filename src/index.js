8e058157184086174f5c9a48661bb8d04478f0e0
Banno <nathan.phillips@banno.com>
2019-04-04 10:18:28 -0500

    Added webpack-boilerplate --devDependencies --new files structure --webpack.config.js file --new scripts in package.json to build and run on webpack-dev-server

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './components/intern-app.html';
---
+++ tools/jha-design-components-loader.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-extra-boolean-cast */
+// This is a Regular Expression we will use to match specific patterns in order to fix their imports.
+const PolymerRegex = /()(class (.*) extends )(Polymer\.Element)/
+
+// This is the logic for the loader. We export it as a module so that it can be used as a loader in Webpack.
+module.exports = (content) => {
+  if (Boolean(content.search(PolymerRegex))) {
+    return content.replace(PolymerRegex, `import { Element as PolymerElement } from '@banno/polymer/polymer-element.js';\n$1$2PolymerElement`);
+  }
+  return content;
+}
