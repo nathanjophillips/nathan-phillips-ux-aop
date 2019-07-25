f6d5b29610d126c9ebcdfd1c94615fde7c992b7a
Banno <nathan.phillips@banno.com>
2019-07-25 11:16:03 -0500

    modified createUser and getUser

--- src/api/local-storage.js
+++ src/api/local-storage.js
@@ -1,50 +1,46 @@
 class localStorageApi {
+
+  static getUsers() {
+    var existingUsers = JSON.parse(localStorage.getItem("allStoredUsers")) || [];
+    console.log("getUsers() current existing stored users = ", existingUsers);
+    return existingUsers;
+ }
  
   static createUser(newUser) {
-      localStorage.setItem("newUser", JSON.stringify(newUser));
-      var existingUsers = localStorageApi.getUsers();
-      existingUsers.push(newUser);
-      localStorage.setItem("allStoredUsers", JSON.stringify(existingUsers));
-      console.log("createUser() new current existing users = ", existingUsers);
-      return existingUsers;
+    const databaseEndpoints = { ALL_USERS: 'allStoredUsers' }
+    var existingUsers = localStorageApi.getUsers();
+    localStorage.setItem(databaseEndpoints.ALL_USERS, JSON.stringify(existingUsers.push(newUser)));
+    console.log("createUser() new current existing users = ", existingUsers);
+    return existingUsers;
   }
 
   static getUser(id) {
-      var existingUsers = localStorageApi.getUsers();
-      var found = existingUsers.find(function(allStoredUsers) {
-          return allStoredUsers.id === id;
-      });
-      console.log('getUser() found = ',found);
-      return found;
+    var existingUsers = localStorageApi.getUsers();
+    var found = existingUsers.find(allStoredUsers => {
+      return allStoredUsers.id === id;
+    });
+    return found;
   }
 
-  static getUsers() {
-     var existingUsers = JSON.parse(localStorage.getItem("allStoredUsers"));
-     if(existingUsers == null) existingUsers = [];
-     console.log("getUsers() current existing stored users = ", existingUsers);
-     return existingUsers;
-  }
-
-  genId() {
-      return '' + Math.random().toString(9).substr(2, 3);
+  static genId() {
+    return '' + Math.random().toString(9).substr(2, 3);
   }
  
   static updateUser(id) {
       // var matchingUser = localStorageApi.getUser(id);
       // console.log(matchingUser);
-
   }
 
   static deleteUser(id) {
-      var deleteThisUser = localStorageApi.getUser(id);
-
-      console.log("deleteUser() the user to delete is =", deleteThisUser);
-      return localStorage.removeItem("allStoredUsers" === deleteThisUser);
+    var deleteThisUser = localStorageApi.getUser(id);
+    // var keyIndex = deleteThisUser.key
+    console.log("deleteUser() the user to delete is =", deleteThisUser);
+    return localStorage.removeItem("allStoredUsers" === deleteThisUser);
   }
 
   static deleteUsers() {
-      localStorage.clear();
-      consol.log("deleteUsers() all users deleted = ", localStorageApi.getUsers() );
+    localStorage.clear();
+    consol.log("deleteUsers() all users deleted = ", localStorageApi.getUsers());
   }
 
 
