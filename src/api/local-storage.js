148085b8255a509f3953639a588f3f5d0b2754e0
Banno <nathan.phillips@banno.com>
2019-07-25 10:24:49 -0500

    console.logs for testing

--- src/api/local-storage.js
+++ src/api/local-storage.js
@@ -5,7 +5,7 @@ class localStorageApi {
       var existingUsers = localStorageApi.getUsers();
       existingUsers.push(newUser);
       localStorage.setItem("allStoredUsers", JSON.stringify(existingUsers));
-      // console.log("createUser() : ", existingUsers);
+      console.log("createUser() new current existing users = ", existingUsers);
       return existingUsers;
   }
 
@@ -14,14 +14,14 @@ class localStorageApi {
       var found = existingUsers.find(function(allStoredUsers) {
           return allStoredUsers.id === id;
       });
-      // console.log(found);
+      console.log('getUser() found = ',found);
       return found;
   }
 
   static getUsers() {
      var existingUsers = JSON.parse(localStorage.getItem("allStoredUsers"));
      if(existingUsers == null) existingUsers = [];
-  //    console.log("getUsers() : ", existingUsers);
+     console.log("getUsers() current existing stored users = ", existingUsers);
      return existingUsers;
   }
 
@@ -38,12 +38,13 @@ class localStorageApi {
   static deleteUser(id) {
       var deleteThisUser = localStorageApi.getUser(id);
 
-      console.log(deleteThisUser);
+      console.log("deleteUser() the user to delete is =", deleteThisUser);
       return localStorage.removeItem("allStoredUsers" === deleteThisUser);
   }
 
   static deleteUsers() {
       localStorage.clear();
+      consol.log("deleteUsers() all users deleted = ", localStorageApi.getUsers() );
   }
 
 
