5e467dca6739535756cd248bb637d99280ea26fd
Banno <nathan.phillips@banno.com>
2019-10-17 10:20:28 -0500

    add firebase-api

--- src/api/firebase-api.js
+++ src/api/firebase-api.js
@@ -4,56 +4,31 @@ import "firebase/database";
 class firebaseApi {
 
   static getUsers() {
-    const existingUsers = firebase.database().ref().child('users');
+    const existingUsers = firebase.database().ref('users/');
     existingUsers.on('value', snap => console.log(snap.val()));
-    console.log(existingUsers);
     return existingUsers;
   }
 
-  // static getUser(userId) {
-  //   return localStorageApi.getUsers().find(storedUsers =>  storedUsers.id === userId );
-  // }
-
-  // static createUser(newUserProfile) {
-  //   const existingUsers = localStorageApi.getUsers();
-  //   existingUsers.push(newUserProfile);
-  //   localStorage.setItem("storedUsers", JSON.stringify(existingUsers));
-  // }
-
-  // static updateUser(userId, newUserData) {
-  //   const existingUsers = localStorageApi.getUsers();
-  //   const newUserProfile = Object.assign(localStorageApi.getUser(userId), newUserData);
-  //   const userKey = existingUsers.findIndex(user => user.id === userId);
-  //   existingUsers.splice(userKey, 1, newUserProfile);
-  //   localStorage.setItem("storedUsers", JSON.stringify(existingUsers));
-  // }
-
-  // static deleteUser(userId) {
-  //   const existingUsers = localStorageApi.getUsers();
-  //   const userKey = existingUsers.findIndex(user => user.id === userId);
-  //   existingUsers.splice(userKey, 1);
-  //   localStorage.setItem("storedUsers", JSON.stringify(existingUsers));
-  // }
-
-  // static deleteUsers() {
-  //   localStorage.clear();
-  //   const existingUsers = localStorageApi.getUsers();
-  //   localStorage.setItem("storedUsers", JSON.stringify(existingUsers));
-  // }
+  static createUser(newUserProfile) {
+    const existingUsers = firebase.database().ref('users/');
+    existingUsers.push(newUserProfile);
+  }
 
   static genId() {
     return '' + Math.random().toString(9).substr(2, 3);
   }
 
-  // static validateUser(user) {
-  //   const existingUsers = localStorageApi.getUsers();
-  //   const existingIds = existingUsers.map(user => user.id); 
-  //   if (!existingIds.includes(user.id)) {
-  //     return true;
-  //   }
-  //   else {
-  //     return console.log("insert attempt failed. "), false;
-  //   }
-  // }
+  static updateUser(userId, newUserData) {
+    firebase.database().ref('users/' + userId).set({newUserData});
+  }
+
+  static deleteUser(userId) {
+    const existingUsers = firebase.database().ref('users/').child(userId);
+    existingUsers.remove();
+  }
+
+  static deleteAll() {
+    firebase.database().ref('/').remove();
+  }
 }
 export default firebaseApi;
