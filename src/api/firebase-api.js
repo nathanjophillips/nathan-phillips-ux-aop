1ee5906f3152577d259823369305c68fe7a889f9
Banno <nathan.phillips@banno.com>
2019-11-25 15:27:20 -0600

    firebase changes

--- src/api/firebase-api.js
+++ src/api/firebase-api.js
@@ -4,14 +4,25 @@ import "firebase/database";
 class firebaseApi {
 
   static getUsers() {
-    const existingUsers = firebase.database().ref('users/');
-    existingUsers.on('value', snap => console.log(snap.val()));
-    return existingUsers;
+    const ref = firebase.database().ref('users');
+    const userList = [];
+    ref.once('value',function(snap) {
+      snap.forEach(function(item) {
+          var itemVal = item.val();
+          userList.push(itemVal);
+      })});
+    console.log("getUsers() userList === ", userList);
+    return userList;
   }
 
+  static getUser(userId) {
+    const userList = firebaseApi.getUsers();
+    return userList.find(users =>  users.child.id === userId );
+  }
+  
   static createUser(newUserProfile) {
-    const existingUsers = firebase.database().ref('users/');
-    existingUsers.push(newUserProfile);
+    firebase.database().ref('users/').push(newUserProfile);
+    firebaseApi.getUsers();
   }
 
   static genId() {
@@ -19,16 +30,22 @@ class firebaseApi {
   }
 
   static updateUser(userId, newUserData) {
-    firebase.database().ref('users/' + userId).set({newUserData});
+    const user = firebaseApi.getUser(userId);
+
+    // firebase.database().ref('users/' + userId).set({newUserData});
+    console.log("updateUser() ran!!!     CurrentArray being returned:", existingUsers);
   }
 
   static deleteUser(userId) {
-    const existingUsers = firebase.database().ref('users/').child(userId);
-    existingUsers.remove();
+    const user = firebaseApi.getUser(userId);
+    console.log("deleting this user!", user);
+    firebaseApi.database().ref('users/').child.child.remove(user);
+    firebaseApi.getUsers();
   }
 
   static deleteAll() {
     firebase.database().ref('/').remove();
+    console.log("daleteAll() ran!!!     CurrentArray being returned:", existingUsers);
   }
 }
 export default firebaseApi;
